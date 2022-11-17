46c3190f11f935b625d3baf3d1c2a197b9e1f009
Martin Kutny <martin@kutny.net>
2022-11-17 01:43:16 +0100

    Add spring cloud discovery capability

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,17 +11,26 @@ group = "net.kutny"
 version = "2.7.7"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
+extra["springCloudVersion"] = "2021.0.5"
+
 repositories {
 	mavenCentral()
 }
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.cloud:spring-cloud-starter")
 
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
 	implementation("de.codecentric:spring-boot-admin-starter-server:2.7.7")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
 }
 
 tasks.withType<KotlinCompile> {
